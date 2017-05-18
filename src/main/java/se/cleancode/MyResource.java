6a8921b0df4674e8d3a627249c0482173c339098
Mikael Sundberg <micke@cleancode.se>
2017-05-18 19:45:22 +0200

    wip

--- src/main/java/se/cleancode/MyResource.java
+++ src/main/java/se/cleancode/MyResource.java
@@ -1,20 +1,79 @@
 package se.cleancode;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.BuildResponseItem;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.command.BuildImageResultCallback;
 import org.apache.maven.cli.MavenCli;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.api.Git;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class MyResource {
 
-    @RequestMapping("/hello")
-    public String mjao() {
+    public String build(String path) {
+        System.out.println("building: "+path);
         MavenCli cli = new MavenCli();
-        cli.doMain(new String[]{"clean", "install"}, "/Users/mikael.sundberg/IdeaProjects/hemma/konstantleverans/", System.out, System.out);
+        cli.doMain(new String[]{"clean", "install"}, path, System.out, System.out);
         return "hello";
     }
 
+    @RequestMapping("/build")
+    public String cloneAndBuild() {
+        String repo = git();
+        build(repo);
+        buildDockerImage(repo);
+        return "done";
+    }
+
+    @RequestMapping("/clone")
+    public String git() {
+
+        try {
+
+            Path git = Paths.get("git");
+            Files.createDirectory(git);
+
+            Git result = Git.cloneRepository()
+                    .setURI("git@github.com:bobo/konstantleverans.git")
+                    .setDirectory(git.toFile())
+                    .call();
+            // Note: the call() returns an opened repository already which needs to be closed to avoid file handle leaks!
+            System.out.println("Having repository: " + result.getRepository().getDirectory());
+            result.close();
+            return result.getRepository().getDirectory().getParentFile().getAbsolutePath();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return "mjao";
+    }
+
+    public void runSubsystemTests(){
+
+    }
+
+    public void buildDockerImage(String path){
+
+        DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+
+        File baseDir = new File(path);
+
+        BuildImageResultCallback callback = new BuildImageResultCallback() {
+            @Override
+            public void onNext(BuildResponseItem item) {
+                System.out.println("" + item);
+                super.onNext(item);
+            }
+        };
+
+        dockerClient.buildImageCmd(baseDir).exec(callback).awaitImageId();
+
+    }
 
 }
